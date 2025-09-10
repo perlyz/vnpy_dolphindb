bc60b049ffcd654bc7fea703b373f25a70c779c7
Peng Zhang <perlyzhang@gmail.com>
2025-09-10 22:11:17 +0800

    change the number of threads to 10
    Signed-off-by: Peng Zhang <perlyzhang@gmail.com>

--- vnpy_dolphindb/dolphindb_database.py
+++ vnpy_dolphindb/dolphindb_database.py
@@ -45,7 +45,7 @@ class DolphindbDatabase(BaseDatabase):
         self.session.connect(self.host, self.port, self.user, self.password)
 
         # 创建连接池（用于数据写入）
-        self.pool: ddb.DBConnectionPool = ddb.DBConnectionPool(self.host, self.port, 1, self.user, self.password)
+        self.pool: ddb.DBConnectionPool = ddb.DBConnectionPool(self.host, self.port, 10, self.user, self.password)
 
         # 初始化数据库和数据表
         if not self.session.existsDatabase(self.db_path):
